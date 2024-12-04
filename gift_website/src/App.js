92dbe812102e7d494bd12ae1cc3367abcecdee17
Josh <153073298+josh-yo@users.noreply.github.com>
2024-12-04 21:42:08 +1000

    Set up environment variables

--- gift_website/src/App.js
+++ gift_website/src/App.js
@@ -4,7 +4,19 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import logo from './logo.svg';
 import './App.css';
 
+import { useEffect } from 'react';
+import axios from 'axios';
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+
 function App() {
+  useEffect(() => {
+    console.log(process.env.REACT_APP_API_URL, process.env.REACT_APP_API_PATH);
+    (async () => {
+      const result = await axios.get(`/v2/api/${process.env.REACT_APP_API_PATH}/products/all`);
+      console.log(result);
+    })()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -12,6 +24,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
