e0114283a84d2f59128388c41dcb8550dd13efbc
Josh <153073298+josh-yo@users.noreply.github.com>
2024-12-05 11:23:12 +1000

    Setup React Router

--- gift_website/src/App.js
+++ gift_website/src/App.js
@@ -6,7 +6,6 @@ import './App.css';
 
 import { useEffect } from 'react';
 import axios from 'axios';
-axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 function App() {
   useEffect(() => {
@@ -19,21 +18,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
--- gift_website/src/index.js
+++ gift_website/src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+import { HashRouter } from 'react-router-dom';
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
