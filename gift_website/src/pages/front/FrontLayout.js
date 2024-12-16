d1625d8cb98ac55736ef3ccdce35c72c9d30d0fa
Josh <153073298+josh-yo@users.noreply.github.com>
2024-12-16 23:57:39 +1000

    Setup basic footer layout and structure

--- gift_website/src/pages/front/FrontLayout.js
+++ gift_website/src/pages/front/FrontLayout.js
@@ -1,32 +1,14 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from '../../components/Navbar';
+import Footer from '../../components/Footer';
 
 function FrontLayout(){
     return(<>
     <Navbar/>
 
     <Outlet></Outlet>
-    
-    {/* footer */}
-    <div className="bg-dark py-5">
-      <div className="container">
-        <div className="d-flex align-items-center justify-content-between text-white mb-md-7 mb-4">
-          <a className="text-white h4" href="./index.html">LOGO</a>
-          <ul className="d-flex list-unstyled mb-0 h4">
-            <li><a href="#" className="text-white mx-3"><i className="fab fa-facebook"></i></a></li>
-            <li><a href="#" className="text-white mx-3"><i className="fab fa-instagram"></i></a></li>
-            <li><a href="#" className="text-white ms-3"><i className="fab fa-line"></i></a></li>
-          </ul>
-        </div>
-        <div className="d-flex flex-column flex-md-row justify-content-between align-items-md-end align-items-start text-white">
-          <div className="mb-md-0 mb-1">
-            <p className="mb-0">02-3456-7890</p>
-            <p className="mb-0">service@mail.com</p>
-          </div>
-          <p className="mb-0">© 2020 LOGO All Rights Reserved.</p>
-        </div>
-      </div>
-    </div>
+
+    <Footer/>
 
     <footer className="footer" 
         style={{  
